79fb9d9282b6c2dfa86439c5f2b60960d6861f3d
yahuimg12-png <yahui.mg12@nycu.edu.tw>
2025-11-25 13:58:31 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -1,10 +1,31 @@
 // 題目 3：破解數學家的謎題 (Cracking the Mathematician's Puzzle) - 30分
 
 #include <iostream>
+#include <cmath>
 using namespace std;
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false; 
 
+    int limit = sqrt(n);
+    for (int i = 3; i <= limit; i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
 int main() {
-    
-    
+    int n;
+    cin >> n;
+
+    for (int a = 2; a <= n / 2; a++) {
+        int b = n - a;
+        if (isPrime(a) && isPrime(b)) {
+            cout << a << " " << b << endl;
+            return 0;
+        }
+    }
+
     return 0;
 }
