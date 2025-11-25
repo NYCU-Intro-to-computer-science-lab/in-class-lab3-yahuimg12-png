cfd7d5a02e52d6c044c6da003b93dabd6d08d3a9
yahuimg12-png <yahui.mg12@nycu.edu.tw>
2025-11-25 14:32:33 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
+void moveDisk(int id, int from, int to) {
+    cout << "Move disk " << id << " from " << from << " to " << to << "\n";
+}
+void hanoi(int n, int from, int to, int aux) {
+
+    if (abs(from - to) == 2) {  
+        hanoi(n, from, aux, to); 
+        return;
+    }
+}
 
 int main() {
-    
-
+    int n;
+    cin >> n;
+    hanoi(n, 2);
     return 0;
 }
