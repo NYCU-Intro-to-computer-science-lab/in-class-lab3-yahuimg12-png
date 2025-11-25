53864e5e7c64f2e98ad0a13c861b1ef7e9246ad1
yahuimg12-png <yahui.mg12@nycu.edu.tw>
2025-11-25 13:46:07 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,10 +1,25 @@
 // 題目 1：高塔建築師 (The Tower Architect) - 20分
 
 #include <iostream>
+
 using namespace std;
 
+long long factorial(int n) {
+    if (n == 0) return 1;     
+    return n * factorial(n - 1);
+}
+
+long long sum(int n) {
+    if (n == 0) return 0;     
+    return n + sum(n - 1);
+}
+
 int main() {
-    
-    
+    int n;
+    cin >> n;
+
+    cout << factorial(n) << endl;
+    cout << sum(n) << endl;
+
     return 0;
 }
